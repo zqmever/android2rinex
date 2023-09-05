22819fea8d606b2a062c955aceb5af57cd336725
zqmever <zqmever@gmail.com>
2023-09-05 19:34:44 +0100

    refactored for efficiency

--- +android/+formatSpec/search.m
+++ +android/+formatSpec/search.m
@@ -1,12 +1,14 @@
-function all_properties = search(data_header)
+function [formatspec_obj, all_formatspecs] = search(data_header)
     % generate the class name
     this_class_name = strcat('android.formatSpec.', data_header);
     % check if the class exists
     if exist(this_class_name, 'class') == 8
-        % if it exists, return its properties
-        all_properties = properties(this_class_name);
+        % if it exists, return the class and its properties
+        formatspec_obj  = feval(this_class_name);
+        all_formatspecs = properties(this_class_name);
     else
         % if it not exists, return an empty cell
-        all_properties = {};
+        formatspec_obj  = [];
+        all_formatspecs = {};
     end
 end
--- +android/fillDataFrame.m
+++ +android/fillDataFrame.m
@@ -4,34 +4,40 @@ function data_frame = fillDataFrame(data_frame, raw_data)
 
     % check if the header is empty
     if isempty(data_frame.header)
+        warning('The header of the data frame for %s is empty. ', data_frame.id);
         return;
     end
 
-    % generate the format spec
-    formatspec_list = android.formatSpec.search(data_frame.id);
-    if ~isempty(formatspec_list)
-        this_formatspec = '';
-        for i = 1:length(data_frame.header)
-            if any(strcmp(formatspec_list, data_frame.header{i}))
-                this_formatspec = strcat(this_formatspec, android.formatSpec.(data_frame.id).(data_frame.header{i}));
-            else
-                this_formatspec = strcat(this_formatspec, '%s');
-            end
+    % check for pre-defined format specs
+    [formatspec_obj, formatspec_list] = android.formatSpec.search(data_frame.id);
+    if isempty(formatspec_obj)
+        warning('Cannot find format specifiers for %s. ', data_frame.id);
+    end
+
+    % generate the format spec string
+    undefined_formatspec = '%s';
+    formatspec_string = '';
+    in_list = ismember(data_frame.header, formatspec_list);
+    for i = 1:length(data_frame.header)
+        if in_list(i)
+            formatspec_string = append(formatspec_string, formatspec_obj.(data_frame.header{i}));
+        else
+            formatspec_string = strcat(formatspec_string, undefined_formatspec);
         end
-    else
+    end
+
+    % check for matched data
+    matched_data_index = strcmp(raw_data{1}, data_frame.id);
+    if ~any(matched_data_index)
+        warning('Cannot find any data for %s. ', data_frame.id);
         return;
     end
 
     % parse the data
-    raw_data_index = strcmp(raw_data{1}, data_frame.id);
-    if any(raw_data_index)
-        this_raw_data = raw_data{2}(raw_data_index);
-        this_raw_cell = cell(size(this_raw_data, 1), length(data_frame.header));
-        for i = 1:size(this_raw_data, 1)
-            this_raw_cell(i,:) = textscan(this_raw_data{i}, this_formatspec, 'Delimiter', ',');
-        end
-        data_frame.data = cell2table(this_raw_cell, 'VariableNames', data_frame.header);
-    else
-        return;
+    matched_data = raw_data{2}(matched_data_index);
+    matched_data_cell = cell(size(matched_data, 1), length(data_frame.header));
+    for i = 1:size(matched_data, 1)
+        matched_data_cell(i,:) = textscan(matched_data{i}, formatspec_string, 'Delimiter', ',');
     end
+    data_frame.data = cell2table(matched_data_cell, 'VariableNames', data_frame.header);
 end
